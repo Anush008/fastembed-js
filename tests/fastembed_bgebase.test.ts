9f00c9c48b354cabc28e6031d6164fc251ad09c9
Anush <anushshetty90@gmail.com>
2023-09-17 20:29:13 +0530

    test: Configure testing (#1)
    * test: configure tests
    
    * ci: configure actions
    
    * ci: syntax fix

---
+++ tests/fastembed_bgebase.test.ts
@@ -0,0 +1,70 @@
+import { expect, test } from 'vitest'
+import { FlagEmbedding, EmbeddingModel } from "../src"
+
+test('Init EmbeddingModel', async () => {
+    const model = await FlagEmbedding.init({
+        modelName: EmbeddingModel.BGEBaseEN
+    });
+    expect(model).toBeDefined();
+});
+
+test("FlagEmbedding embed", async () => {
+  const flagEmbedding = await FlagEmbedding.init({
+    modelName: EmbeddingModel.BGEBaseEN,
+    maxLength: 512,
+  });
+  const embeddings = (await flagEmbedding.embed(["This is a test"]).next())
+    .value!;
+  expect(embeddings).toBeDefined();
+  expect(embeddings.length).toBe(1);
+});
+
+test("FlagEmbedding embed batch", async () => {
+  const flagEmbedding = await FlagEmbedding.init({
+    modelName: EmbeddingModel.BGEBaseEN,
+
+    maxLength: 512,
+  });
+  const embeddingsBatch = flagEmbedding.embed(["This is a test", "Some text", "Some more test", "This is a test", "Some text", "Some more test"]);
+  for await (const embeddings of embeddingsBatch) {
+    expect(embeddings).toBeDefined();
+    expect(embeddings.length).toBe(6);
+    expect(embeddings[0].length).toBe(768);
+  }
+});
+
+test("FlagEmbedding embed Base batch", async () => {
+  const flagEmbedding = await FlagEmbedding.init({
+    modelName: EmbeddingModel.BGEBaseEN,
+    maxLength: 512,
+  });
+  const embeddingsBatch = flagEmbedding.embed(["This is a test", "Some text", "Some more test", "This is a test", "Some text", "Some more test"], 1);
+  for await (const embeddings of embeddingsBatch) {
+    expect(embeddings).toBeDefined();
+    expect(embeddings.length).toBe(1);
+    expect(embeddings[0].length).toBe(768);
+  }
+});
+
+test("FlagEmbedding queryEmbed", async () => {
+  const flagEmbedding = await FlagEmbedding.init({
+    modelName: EmbeddingModel.BGEBaseEN,
+    maxLength: 512,
+  });
+  const embeddings = await flagEmbedding.queryEmbed("This is a test");
+  expect(embeddings).toBeDefined();
+  expect(embeddings.length).toBe(1);
+});
+
+test("FlagEmbedding passageEmbed", async () => {
+  const flagEmbedding = await FlagEmbedding.init({
+    modelName: EmbeddingModel.BGEBaseEN,
+
+    maxLength: 512,
+  });
+  const embeddings = (
+    await flagEmbedding.passageEmbed(["This is a test"]).next()
+  ).value!;
+  expect(embeddings).toBeDefined();
+  expect(embeddings.length).toBe(1);
+});
